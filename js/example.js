4e2d4734b19373c7b628a903ade31d648c1e6580
Yoriiis <joris.daniel@gmail.com>
2018-04-22 23:36:48 +0200

    First commit Version 1.1.0

---
+++ js/example.js
@@ -0,0 +1,27 @@
+//Youtube example
+var playerYT = new vLite({
+    selector: '#player-yt-1',
+    callback: (player) => {
+        //Ready
+    }
+});
+
+//HTML5 example
+var playerHtml5 = new vLite({
+    selector: '#player-html5-1',
+    options: {
+        "autoplay": false,
+        "controls": true,
+        "playPause": true,
+        "time": true,
+        "timeline": true,
+        "volume": true,
+        "fullscreen": true,
+        "poster": "img/poster.jpg",
+        "bigPlay": true,
+        "nativeControlsForTouch": true
+    },
+    callback: (player) => {
+        //Ready
+    }
+});
