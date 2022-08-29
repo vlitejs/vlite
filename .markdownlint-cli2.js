6b3ac25f5c912be0591ed23e10fdac48ee3c3c6b
Yoriiis <joris.daniel@gmail.com>
2022-08-29 22:21:45 +0200

    Update markdownlint config

--- .markdownlint-cli2.js
+++ .markdownlint-cli2.js
@@ -4,7 +4,8 @@ module.exports = {
 	config: {
 		MD013: false, // Line length
 		MD024: false, // Multiple headers with the same content
-		MD033: false // No inline HTML
+		MD033: false, // No inline HTML
+		MD036: false // No emphasis as header
 	},
 	ignores: ['**/node_modules/**']
 }
