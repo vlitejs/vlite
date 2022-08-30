a1865386f8f9174c93989347535b325e42a74c0e
Yoriiis <joris.daniel@gmail.com>
2022-08-30 19:23:55 +0200

    Move tsconfig to config directory Move GitHub docs to .github directory Update markdown lint

--- .markdownlint-cli2.js
+++ .markdownlint-cli2.js
@@ -7,5 +7,5 @@ module.exports = {
 		MD033: false, // No inline HTML
 		MD036: false // No emphasis as header
 	},
-	ignores: ['**/node_modules/**']
+	ignores: ['**/node_modules/**', '.github/PULL_REQUEST_TEMPLATE.md']
 }
