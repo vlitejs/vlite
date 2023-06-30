e88e83b4b8965287c611cec4ea01d522bcf66bcd
Yoriiis <joris.daniel@gmail.com>
2023-07-01 01:35:44 +0200

    Remove stylelint and markdown

--- .markdownlint-cli2.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	config: {
-		MD013: false, // Line length
-		MD024: false, // Multiple headers with the same content
-		MD033: false, // No inline HTML
-		MD036: false // No emphasis as header
-	},
-	ignores: ['**/node_modules/**', '.github/PULL_REQUEST_TEMPLATE.md']
-}
