6ecff97607b63687c74b18b7cfa9e0b091f93ee1
Yoriiis <joris.daniel@gmail.com>
2023-07-01 22:04:51 +0200

    Update stylelint config

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
 	rules: {
-		indentation: ['tab', { ignore: 'inside-parens' }],
 		'unit-allowed-list': ['em', 'rem', '%', 'px', 's', 'deg', 'fr', 'vh', 'vw', 'ms'],
 		'declaration-colon-newline-after': null,
 		'value-list-comma-newline-after': null,
