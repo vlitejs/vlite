e6ed553fc042e09cbd47394720012f8fff8a628e
Yoriiis <joris.daniel@gmail.com>
2023-07-01 02:38:38 +0200

    Update eslint

---
+++ npm.bash
@@ -0,0 +1,10 @@
+npm install @typescript-eslint/eslint-plugin@5.59.0 \
+                @typescript-eslint/parser@5.59.0 \
+                eslint@8.39.0 \
+                eslint-config-prettier@8.8.0 \
+                eslint-config-standard@17.0.0 \
+                eslint-plugin-import@2.27.5 \
+                eslint-plugin-n@15.7.0 \
+                eslint-plugin-node@11.1.0 \
+                eslint-plugin-prettier@4.2.1 \
+                eslint-plugin-promise@6.1.1 --save-dev
