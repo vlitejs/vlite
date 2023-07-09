fcf8616cdad440b0ef8ad42c380126ed06452b63
Yoriiis <joris.daniel@gmail.com>
2023-07-09 16:16:26 +0200

    Fis postcss with rollup

--- config/postcss.config.cjs
+++ config/postcss.config.cjs
@@ -1,26 +1,22 @@
 module.exports = {
-	plugins: [
-		'postcss-import',
-		'postcss-url',
-		'postcss-nested',
-		[
-			'postcss-preset-env',
-			{
-				stage: 2,
-				features: {
-					'nesting-rules': false,
-					'custom-properties': {
-						warnings: true,
-						preserve: true
-					}
+	plugins: {
+		'postcss-import': {},
+		'postcss-url': {},
+		'postcss-nested': {},
+
+		'postcss-preset-env': {
+			stage: 2,
+			features: {
+				'nesting-rules': false,
+				'custom-properties': {
+					warnings: true,
+					preserve: true
 				}
 			}
-		],
-		[
-			'postcss-custom-media',
-			{
-				preserve: false
-			}
-		]
-	]
+		},
+
+		'postcss-custom-media': {
+			preserve: false
+		}
+	}
 }
