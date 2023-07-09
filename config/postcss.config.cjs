41cc75d3d52232d344d6cccb1e76fa46a962b317
Yoriiis <joris.daniel@gmail.com>
2023-07-09 16:07:06 +0200

    Update postcss config

--- config/postcss.config.cjs
+++ config/postcss.config.cjs
@@ -1,20 +1,26 @@
 module.exports = {
 	plugins: [
-		require('postcss-import'),
-		require('postcss-url'),
-		require('postcss-preset-env')({
-			stage: 2,
-			features: {
-				'nesting-rules': false,
-				'custom-properties': {
-					warnings: true,
-					preserve: true
+		'postcss-import',
+		'postcss-url',
+		'postcss-nested',
+		[
+			'postcss-preset-env',
+			{
+				stage: 2,
+				features: {
+					'nesting-rules': false,
+					'custom-properties': {
+						warnings: true,
+						preserve: true
+					}
 				}
 			}
-		}),
-		require('postcss-nested'),
-		require('postcss-custom-media')({
-			preserve: false
-		})
+		],
+		[
+			'postcss-custom-media',
+			{
+				preserve: false
+			}
+		]
 	]
 }
