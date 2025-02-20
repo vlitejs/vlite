560b04f4520f9165b586530a709b714a6df47324
Yoriiis <2563298+yoriiis@users.noreply.github.com>
2025-02-20 20:54:04 +0100

    Add prettier for markdown and html files

---
+++ config/prettier.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+	printWidth: 100,
+	useTabs: true,
+	semi: false,
+	trailingComma: 'none',
+	singleQuote: true,
+	arrowParens: 'always',
+	endOfLine: 'lf',
+	overrides: [
+		{
+			files: '*.md',
+			options: {
+				proseWrap: 'preserve',
+				tabWidth: 2,
+				useTabs: false,
+				singleQuote: true,
+				semi: true
+			}
+		},
+		{
+			files: '*.html',
+			options: {
+				printWidth: 500
+			}
+		}
+	]
+}
